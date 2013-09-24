97002ca79203b43047e69c590c421f5c94d63dcc
cjf314 <cjf314@gmail.com>
2013-09-23 21:34:34 -0500

    A few tweaks to connection

--- CommunicationProtocol/communicationprotocol/src/com/mutableconst/protocol/Connection.java
+++ CommunicationProtocol/communicationprotocol/src/com/mutableconst/protocol/Connection.java
@@ -1,78 +1,130 @@
-package com.mutableconst.android.dashboard_manager;
+package com.mutableconst.protocol;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.lang.reflect.Method;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-import android.app.Activity;
-import android.app.PendingIntent;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.telephony.SmsManager;
-import android.widget.Toast;
+public class Connection {
 
-import com.mutableconst.chatserver.gui.MainActivity;
-import com.mutableconst.protocol.Protocol;
-
-public class AndroidConnection {
-
+	private final static int PORT = 7767;
 	private final static char NEW_LINE = '\n';
 	private final static String PING_STRING = "";
 	private final static int MAX_PING_COUNTER = 30;
-	private final static int PORT = 7767;
+	private final static String ANDROID_EVENT_MANAGER_CLASS = "com.mutableconst.android.dashboard_manager.AndroidEventManager";
+	private final static String DESKTOP_EVENT_MANAGER_CLASS = "com.mutableconst.dashboard_manager.EventManager";
 
 	private static int pingCounter = MAX_PING_COUNTER;
-
-	private static boolean started = false;
-
+	private static boolean running = false;
+	private static boolean isServer;
 	private static Socket socket;
+	private static ServerSocket serverSocket;
 	private static BufferedReader in;
 	private static PrintWriter out;
 
-	private static String serverAddress = "192.168.1.132";
+	private static String serverAddress = "192.168.1.131";
 
 	private final static StringBuilder responseBuilder = new StringBuilder();
 	private final static ConcurrentLinkedQueue<String> requests = new ConcurrentLinkedQueue<String>();
 
-	public static void startAndroidConnection() {
-		if (!started) {
-			started = true;
-			new AndroidConnection();
+	private static Thread connectionThread;
+
+	private static int extraSleep;
+
+	private static Class<?> eventManager;
+	private static Method handleResponseMethod;
+	private static Class<?>[] handleResponseParameter = new Class[] { Protocol.class };
+
+	/*
+	 * If a connection currently isn't running, setup all needed values for the
+	 * Connection and start it up.
+	 */
+	public static void startConnection(boolean server) {
+		if (!running) {
+			running = true;
+			isServer = server;
+			/*
+			 * Setup the reflection EventManager and the reflection method
+			 * handleResponse to use on this Connection.
+			 */
+			try {
+				if (isServer) {
+					eventManager = Class.forName(ANDROID_EVENT_MANAGER_CLASS);
+				} else {
+					eventManager = Class.forName(DESKTOP_EVENT_MANAGER_CLASS);
+				}
+				handleResponseMethod = eventManager.getDeclaredMethod("handleResponse", handleResponseParameter);
+			} catch (Exception e) {
+				System.out.println("Connection could not find an EventManager or Method to use. Aborting Connection");
+				e.printStackTrace();
+			}
+			manageConnection();
 		}
 	}
 
-	private AndroidConnection() {
-		new Thread(new Runnable() {
+	/*
+	 * Sends a trigger to the current Connection Thread that it needs to stop.
+	 */
+	public static void stopConnection() {
+		running = false;
+	}
+
+	/*
+	 * Stops the current Connection and starts it up again
+	 */
+	public static void restartConnection() {
+		if (running) {
+			stopConnection();
+			startConnection(isServer);
+		}
+	}
+
+	/*
+	 * Add an outgoing request to the socket. Requests is an interally
+	 * synchronized queue.
+	 */
+	public static boolean addRequest(String request) {
+		if (running) {
+			requests.add(request);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/*
+	 * Spawn's a new thread to handle the connection state and IO in. Any
+	 * attempts at closing the socket will be repeatedly recovered from.
+	 */
+	private static void manageConnection() {
+		connectionThread = new Thread(new Runnable() {
 			@Override
 			public void run() {
-				while (true) {
+				while (running) {
 					try {
-						Thread.sleep(500);
+						Thread.sleep(500 + extraSleep);
+						extraSleep = 0;
 						setupSocket();
 						processSocketOutput();
 						processSocketInput();
 						pingSocket();
 					} catch (SocketException e) {
-						Preferences.sendToast("Socket is null: " + e.toString());
 						resetSocket();
-					} catch (UnknownHostException e) {
-						Preferences.sendToast(e.toString());
+					} catch (Exception e) {
+						System.out.println("Exception on Socket: " + e.toString());
 						e.printStackTrace();
-					} catch (IOException e) {
-						Preferences.sendToast(e.toString());
-						e.printStackTrace();
-					} catch (InterruptedException e) {
-						Preferences.sendToast(e.toString());
-						e.printStackTrace();
+						extraSleep = 5000; // To slow down repeated attempts on the socket.
 					}
 				}
 			}
-		}).start();
+		});
+		connectionThread.start();
 	}
 
 	/*
@@ -82,23 +134,42 @@ public class AndroidConnection {
 	 */
 	private static void setupSocket() throws UnknownHostException, IOException {
 		if (socket == null) {
-			Preferences.sendToast("Creating a New Socket");
-			serverAddress = Preferences.getPreference(Preferences.SERVER_IP_ADDRESS);
-			socket = new Socket(serverAddress, PORT);
+			if (isServer) {
+				serverSocket = new ServerSocket(PORT);
+				socket = serverSocket.accept();
+			} else {
+				socket = new Socket(serverAddress, PORT);
+			}
 			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			out = new PrintWriter(socket.getOutputStream(), true);
+			System.out.println("Connection Made!");
 		}
 	}
 
-	private static void processSocketOutput() {
-		if (!requests.isEmpty() && out != null) {
-			Preferences.sendToast("Outputing Data: " + requests.peek());
-			out.println(requests.poll());
+	/*
+	 * Checks the requests ConcurrentLinkedQueued for any requests that have
+	 * been added. While the requests queue has requests pending they will be
+	 * written to the socket. If the socket's error state ever switches to true,
+	 * this method will throw a SocketException
+	 */
+	private static void processSocketOutput() throws SocketException {
+		while (!requests.isEmpty() && out != null) {
+			out.println(requests.peek());
+			if (out.checkError()) {
+				throw new SocketException("Output failed!");
+			} else {
+				requests.poll();
+			}
 		}
 	}
 
+	/*
+	 * Reads the character stream from the socket while their are incoming
+	 * characters and the socket doesn't block the current thread's execution.
+	 * Passes completed requests to the appropriate EventManager.
+	 */
 	private static void processSocketInput() throws IOException {
-		while (in != null && in.ready()) {
+		while(in.ready() && in != null) {
 			char nextChar = (char) in.read();
 			responseBuilder.append(nextChar);
 			if (nextChar == NEW_LINE) {
@@ -106,12 +177,29 @@ public class AndroidConnection {
 				System.out.println("Incoming request: " + rawResponse);
 				responseBuilder.setLength(0);
 				if (rawResponse.length() > 0) {
-					AndroidEventManager.handleResponse(new Protocol(rawResponse));
+					reflectEvent(new Protocol(rawResponse));
 				}
 			}
 		}
 	}
 
+	/*
+	 * Dynamically call either the Desktop or Android EventManager
+	 * handleResponse method with requests incoming on the socket. Reflection
+	 * was used to prevent having to compile both projects into each other.
+	 */
+	private static void reflectEvent(Protocol protocol) {
+		try {
+			handleResponseMethod.invoke(null, (Object) protocol);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/*
+	 * Send an empty string on the socket. Throws a SocketException if the
+	 * socket error state is set after the ping.
+	 */
 	private static void pingSocket() throws SocketException {
 		if (pingCounter-- == 0) {
 			pingCounter = MAX_PING_COUNTER;
@@ -122,18 +210,14 @@ public class AndroidConnection {
 		}
 	}
 
+	/*
+	 * Force closes the socket, the socket's input, and the socket's output
+	 * streams.
+	 */
 	private static void resetSocket() {
 		socket = null;
 		in = null;
 		out = null;
 	}
 
-	public static boolean addRequest(String request) {
-		if (requests != null) {
-			requests.add(request);
-			return true;
-		} else {
-			return false;
-		}
-	}
 }
